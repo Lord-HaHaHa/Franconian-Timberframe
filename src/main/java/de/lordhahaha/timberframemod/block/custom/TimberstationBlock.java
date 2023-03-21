288e60ec999e66353f372de957967515be6ea3b4
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-03-21 10:17:18 +0100

    Update TimberstationBlock.java

--- src/main/java/de/lordhahaha/timberframemod/block/custom/TimberstationBlock.java
+++ src/main/java/de/lordhahaha/timberframemod/block/custom/TimberstationBlock.java
@@ -1,20 +1,38 @@
 package de.lordhahaha.timberframemod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.inventory.StonecutterMenu;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 
 public class TimberstationBlock extends Block {
+    private static final Component CONTAINER_TITLE = Component.translatable("container.crafting");
 
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public TimberstationBlock(Properties properties) {
@@ -26,6 +44,25 @@ public class TimberstationBlock extends Block {
             Block.box(14, 0, 14, 16, 14, 16),
             Block.box(0, 0, 14, 2, 14, 16),
             Block.box(0, 0, 0, 2, 14, 2));
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        if (!level.isClientSide && player instanceof ServerPlayer serverPlayer) {
+            System.out.println("Open Menu");
+            NetworkHooks.openScreen(serverPlayer, this.getMenuProvider(state, level, pos));
+            System.out.println("After Hook open Screen");
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide);
+    }
+
+
+    @Nullable
+    @Override
+    public MenuProvider getMenuProvider(BlockState blockState, Level level, BlockPos blockPos) {
+        return new SimpleMenuProvider((p_57074_, p_57075_, p_57076_) -> {
+            return new CraftingMenu(p_57074_, p_57075_, ContainerLevelAccess.create(level, blockPos));
+        }, CONTAINER_TITLE);
+    }
 
     @Override
     public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
