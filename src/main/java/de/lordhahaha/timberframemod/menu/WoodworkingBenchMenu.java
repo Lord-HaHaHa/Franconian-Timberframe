2feb879f77cd26725a02ae5a9bcbbcb3447c37c5
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-03-29 13:40:12 +0200

    Recipes now need correct order and Count

--- src/main/java/de/lordhahaha/timberframemod/menu/WoodworkingBenchMenu.java
+++ src/main/java/de/lordhahaha/timberframemod/menu/WoodworkingBenchMenu.java
@@ -1,5 +1,6 @@
 package de.lordhahaha.timberframemod.menu;
 
+import ca.weblite.objc.Message;
 import com.google.common.collect.Lists;
 import de.lordhahaha.timberframemod.block.ModBlocks;
 import de.lordhahaha.timberframemod.recipe.WoodworkingBenchRecipe;
@@ -15,6 +16,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 public class WoodworkingBenchMenu extends AbstractContainerMenu {
@@ -33,6 +35,7 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
     public final Container containerForBlock = new SimpleContainer(1) {
         public void setChanged() {
             super.setChanged();
+            System.out.println("containerForBlock: Changed");
             WoodworkingBenchMenu.this.slotsChanged(this);
             WoodworkingBenchMenu.this.slotUpdateListener.run();
         }
@@ -40,6 +43,7 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
     public final Container containerForExtra = new SimpleContainer(1) {
         public void setChanged() {
             super.setChanged();
+            System.out.println("containerForExtra: Changed");
             WoodworkingBenchMenu.this.slotsChanged(this);
             WoodworkingBenchMenu.this.slotUpdateListener.run();
         }
@@ -57,16 +61,26 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
         this.inputSlotBlock = this.addSlot(new Slot(this.containerForBlock, 0, 20, 33));
         this.inputSlotExtra = this.addSlot(new Slot(this.containerForExtra, 0, 20, 52));
         this.resultSlot = this.addSlot(new Slot(this.resultContainer, 1, 143, 33) {
-            public boolean mayPlace(ItemStack p_40362_) {
+            public boolean mayPlace(ItemStack itemStack) {
                 return false;
             }
 
             public void onTake(Player player, ItemStack itemStack) {
                 itemStack.onCraftedBy(player.level, player, itemStack.getCount());
                 WoodworkingBenchMenu.this.resultContainer.awardUsedRecipes(player);
-                ItemStack itemstack = WoodworkingBenchMenu.this.inputSlotBlock.remove(1);
-                ItemStack itemStackExtra = WoodworkingBenchMenu.this.inputSlotExtra.remove(1);
+
+                WoodworkingBenchRecipe recipe = recipes.get(getSelectedRecipeIndex());
+
+                System.out.println(MessageFormat.format("onTake: Selected Index before Remove: {0}", getSelectedRecipeIndex()));
+                System.out.println(MessageFormat.format("onTake: Recipe: {0}", recipes.get(getSelectedRecipeIndex()).getIngredients().get(0).getItems()));
+                System.out.println(MessageFormat.format("onTake: Amount: Remove Slot1: {0} | Remove Slot2 {1}", recipe.amountIngredient1, recipe.amountIngredient2));
+                ItemStack itemstack = WoodworkingBenchMenu.this.inputSlotBlock.remove(recipe.amountIngredient1);
+                System.out.println(recipes.size());
+                System.out.println(MessageFormat.format("onTake: Selected Index: {0} | Max Recipes: {1}", getSelectedRecipeIndex(), getNumRecipes()));
+                //System.out.println(recipes.get(getSelectedRecipeIndex()).getIngredients());
+                ItemStack itemStackExtra = WoodworkingBenchMenu.this.inputSlotExtra.remove(recipe.amountIngredient2);
                 if (!itemstack.isEmpty() && !itemStackExtra.isEmpty()) {
+                    System.out.println("onTake: SetupResultSlot");
                     WoodworkingBenchMenu.this.setupResultSlot();
                 }
 
@@ -99,9 +113,10 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
         return stillValid(this.access, player, ModBlocks.BLOCK_WOODWORKING_BENCH.get());
     }
 
-    public boolean clickMenuButton(Player player, int p_40310_) {
-        if (this.isValidRecipeIndex(p_40310_)) {
-            this.selectedRecipeIndex.set(p_40310_);
+    public boolean clickMenuButton(Player player, int index) {
+        if (this.isValidRecipeIndex(index)) {
+            this.selectedRecipeIndex.set(index);
+            System.out.println(MessageFormat.format("Selected Index: {0} | {1}", index, getSelectedRecipeIndex()));
             this.setupResultSlot();
         }
 
@@ -128,26 +143,28 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
 
     private void setupRecipeList(Container container1, ItemStack itemStackBlock) {
         this.recipes.clear();
+        System.out.println("setupRecipeList: Set RecipeIndex to -1");
         this.selectedRecipeIndex.set(-1);
+
         this.resultSlot.set(ItemStack.EMPTY);
 
-        SimpleContainer container = new SimpleContainer(2);
-        container.setItem(0, this.containerForBlock.getItem(0));
-        container.setItem(1, this.containerForExtra.getItem(0));
+        SimpleContainer container = getInputContainer();
+
         if (!itemStackBlock.isEmpty()) {
             this.recipes = this.level.getRecipeManager().getRecipesFor(WoodworkingBenchRecipe.Type.INSTANCE, container, this.level);
-            System.out.println(container.getContainerSize());
-            System.out.println(container.getItem(0));
-            System.out.println(this.recipes.size());
+            System.out.println(MessageFormat.format("setupRecipeList: Recipes Length: {0}", this.recipes.size()));
         }
     }
 
     void setupResultSlot() {
         if (!this.recipes.isEmpty() && this.isValidRecipeIndex(this.selectedRecipeIndex.get())) {
+            System.out.println("setUpResultSlot: Setup Result Slot");
             WoodworkingBenchRecipe woodworkingBenchRecipe = this.recipes.get(this.selectedRecipeIndex.get());
             this.resultContainer.setRecipeUsed(woodworkingBenchRecipe);
-            this.resultSlot.set(woodworkingBenchRecipe.assemble(this.containerForBlock));
+            this.resultSlot.set(woodworkingBenchRecipe.assemble(getInputContainer()));
         } else {
+            System.out.println("setUpResultSlot: Clear Field");
+            System.out.println(MessageFormat.format("setUpResultSlot: Recipes.Empty() {0} | SelectedRecipe Index: {1}", this.recipes.isEmpty(), this.selectedRecipeIndex.get()));
             this.resultSlot.set(ItemStack.EMPTY);
         }
 
@@ -217,5 +234,11 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
     public List<WoodworkingBenchRecipe> getRecipes() {return this.recipes;}
     public int getSelectedRecipeIndex() {return this.selectedRecipeIndex.get();}
     public void registerUpdateListener(Runnable runnable) {this.slotUpdateListener = runnable;}
+    private SimpleContainer getInputContainer(){
+        SimpleContainer container = new SimpleContainer(2);
+        container.setItem(0, this.containerForBlock.getItem(0));
+        container.setItem(1, this.containerForExtra.getItem(0));
+        return container;
+    }
 
 }
--- src/main/java/de/lordhahaha/timberframemod/recipe/WoodworkingBenchRecipe.java
+++ src/main/java/de/lordhahaha/timberframemod/recipe/WoodworkingBenchRecipe.java
@@ -1,5 +1,7 @@
 package de.lordhahaha.timberframemod.recipe;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import de.lordhahaha.timberframemod.Timberframemod;
 import de.lordhahaha.timberframemod.block.ModBlocks;
@@ -14,19 +16,40 @@ import net.minecraft.world.level.Level;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+
 public class WoodworkingBenchRecipe extends SingleItemRecipe {
-    public WoodworkingBenchRecipe(ResourceLocation id, String group, Ingredient ingredient, ItemStack itemStack) {
-        super(Type.INSTANCE, Serializer.INSTANCE, id, group, ingredient, itemStack);
+    public int amountIngredient1;
+    public int amountIngredient2;
+    public Ingredient ingredientExtra;
+    public WoodworkingBenchRecipe(ResourceLocation id, String group, Ingredient ingredientMain, Ingredient pingredientExtra, ItemStack itemStack, int amount1, int amount2) {
+        super(Type.INSTANCE, Serializer.INSTANCE, id, group, ingredientMain, itemStack);
+        amountIngredient1 = amount1;
+        amountIngredient2 = amount2;
+        ingredientExtra = pingredientExtra;
     }
 
-    public boolean matches(Container p_44483_, Level p_44484_) {
-        return this.ingredient.test(p_44483_.getItem(0));
+    public boolean matches(Container container, Level p_44484_) {
+        ItemStack itemStack1 = container.getItem(0);
+        ItemStack itemStack2 = container.getItem(1);
+        System.out.println(MessageFormat.format("RecipeMatch: {0}", this.ingredient.test(itemStack1)));
+        System.out.println(MessageFormat.format("RecipeMatch: {0}", itemStack1.getCount() >= amountIngredient1));
+        System.out.println(MessageFormat.format("RecipeMatch: {0}", this.ingredient.test(itemStack2)));
+        System.out.println(MessageFormat.format("RecipeMatch: {0}", itemStack2.getCount() >= amountIngredient2));
+        boolean flag = (this.ingredient.test(itemStack1) && itemStack1.getCount() >= amountIngredient1) &&
+                (this.ingredientExtra.test(itemStack2) && itemStack2.getCount() >= amountIngredient2);
+        System.out.println(MessageFormat.format("RecipeMatch: Flag: {0}", flag));
+        return flag;
     }
 
     public ItemStack getToastSymbol() {
         return new ItemStack(ModBlocks.BLOCK_WOODWORKING_BENCH.get());
     }
 
+    public int[] getIngredientsAmount(){
+        return new int[]{amountIngredient1, amountIngredient2};
+    }
     public static class Type implements RecipeType<WoodworkingBenchRecipe>{
         private Type() {}
         public static final Type INSTANCE = new Type();
@@ -39,28 +62,44 @@ public class WoodworkingBenchRecipe extends SingleItemRecipe {
 
         @Override
         public WoodworkingBenchRecipe fromJson(ResourceLocation id, JsonObject recipe) {
-            Ingredient ingredient = null;
-
+            int amounts[] = {0,0};
+            ArrayList<Ingredient> ingredients = new ArrayList<>();
             if(GsonHelper.isArrayNode(recipe, "ingredient")){
-                ingredient = Ingredient.fromJson(GsonHelper.getAsJsonArray(recipe, "ingredient"));
+                JsonArray ingredientArray = GsonHelper.getAsJsonArray(recipe, "ingredient");
+                for(int i = 0; i <= ingredientArray.size()-1; i++){
+                    JsonObject ingredientObject = ingredientArray.get(i).getAsJsonObject();
+                    System.out.println(MessageFormat.format("Recipe Read JSON: ingElement: {0}", ingredientObject));
+                    Ingredient ingredient = Ingredient.fromJson(ingredientObject);
+                    ingredients.add(ingredient);
+                    int amount = ingredientObject.get("count").getAsInt();
+                    amounts[i] = amount;
+                    System.out.println(MessageFormat.format("Recipe Read JSON: ingredient: {0} | Amount: {1}", ingredient, amount));
+                }
+                //ingredient = Ingredient.fromJson(GsonHelper.getAsJsonArray(recipe, "ingredient"));
             }
 
             String s1 = GsonHelper.getAsString(recipe, "result");
             int resultCount = GsonHelper.getAsInt(recipe, "count");
             ItemStack itemStack = new ItemStack(Registry.ITEM.get(new ResourceLocation(s1)) , resultCount);
-            return new WoodworkingBenchRecipe(id, "wood_working", ingredient, itemStack);
+            return new WoodworkingBenchRecipe(id, "wood_working", ingredients.get(0), ingredients.get(1), itemStack, amounts[0], amounts[1]);
         }
 
         @Override
         public @Nullable WoodworkingBenchRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buff) {
             Ingredient ingredient = Ingredient.fromNetwork(buff);
+            Ingredient ingredientExtra = Ingredient.fromNetwork(buff);
+            int amountIngredient1 = buff.readInt();
+            int amountIngredient2 = buff.readInt();
             ItemStack itemStack = buff.readItem();
-            return new WoodworkingBenchRecipe(id, "wood_working", ingredient, itemStack);
+            return new WoodworkingBenchRecipe(id, "wood_working", ingredient, ingredientExtra, itemStack, amountIngredient1, amountIngredient2);
         }
 
         @Override
         public void toNetwork(FriendlyByteBuf buff, WoodworkingBenchRecipe recipe) {
             recipe.ingredient.toNetwork(buff);
+            recipe.ingredientExtra.toNetwork(buff);
+            buff.writeInt(recipe.amountIngredient1);
+            buff.writeInt(recipe.amountIngredient2);
             buff.writeItem(recipe.result);
         }
     }
