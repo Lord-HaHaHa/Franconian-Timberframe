fe2f2899160f5c0794b5c1f06871b784f30e5813
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-04-02 18:51:10 +0200

    Dont lose item when closing menu

--- src/main/java/de/lordhahaha/timberframemod/menu/WoodworkingBenchMenu.java
+++ src/main/java/de/lordhahaha/timberframemod/menu/WoodworkingBenchMenu.java
@@ -35,7 +35,6 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
     public final Container containerForBlock = new SimpleContainer(1) {
         public void setChanged() {
             super.setChanged();
-            System.out.println("containerForBlock: Changed");
             WoodworkingBenchMenu.this.slotsChanged(this);
             WoodworkingBenchMenu.this.slotUpdateListener.run();
         }
@@ -43,7 +42,6 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
     public final Container containerForExtra = new SimpleContainer(1) {
         public void setChanged() {
             super.setChanged();
-            System.out.println("containerForExtra: Changed");
             WoodworkingBenchMenu.this.slotsChanged(this);
             WoodworkingBenchMenu.this.slotUpdateListener.run();
         }
@@ -91,6 +89,7 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
             }
         });
 
+        // Add Inventory Slots
         for(int i = 0; i < 3; ++i) {
             for(int j = 0; j < 9; ++j) {
                 this.addSlot(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
@@ -138,7 +137,6 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
 
     private void setupRecipeList(Container container1, ItemStack itemStackBlock) {
         this.recipes.clear();
-        System.out.println("setupRecipeList: Set RecipeIndex to -1");
         this.selectedRecipeIndex.set(-1);
 
         this.resultSlot.set(ItemStack.EMPTY);
@@ -167,58 +165,64 @@ public class WoodworkingBenchMenu extends AbstractContainerMenu {
         return slot.container != this.resultContainer && super.canTakeItemForPickAll(itemStack, slot);
     }
 
-    public ItemStack quickMoveStack(Player player, int slotIndex) {
-        System.out.println(MessageFormat.format("quickMoveStack: used slotIndex: {0}", slotIndex));
-        ItemStack itemstack = ItemStack.EMPTY;
-        Slot slot = this.slots.get(slotIndex);
-        if (slot != null && slot.hasItem()) {
-            ItemStack itemstack1 = slot.getItem();
-            Item item = itemstack1.getItem();
-            itemstack = itemstack1.copy();
-            if (slotIndex == 3) {
-                item.onCraftedBy(itemstack1, player.level, player);
-                if (!this.moveItemStackTo(itemstack1, 2, 38, true)) {
-                    return ItemStack.EMPTY;
-                }
+    // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
+    // must assign a slot number to each of the slots used by the GUI.
+    // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
+    // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
+    //  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
+    //  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
+    //  36 - 44 = TileInventory slots, which map to our TileEntity slot numbers 0 - 8)
+    private static final int HOTBAR_SLOT_COUNT = 9;
+    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
+    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
+    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
+    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
+    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
 
-                slot.onQuickCraft(itemstack1, itemstack);
-            } else if (slotIndex == 0) {
-                if (!this.moveItemStackTo(itemstack1, 2, 38, false)) {
-                    return ItemStack.EMPTY;
-                }
-            } else if (this.level.getRecipeManager().getRecipeFor(WoodworkingBenchRecipe.Type.INSTANCE, new SimpleContainer(itemstack1), this.level).isPresent()) {
-                if (!this.moveItemStackTo(itemstack1, 0, 1, false)) {
-                    return ItemStack.EMPTY;
-                }
-            } else if (slotIndex >= 2 && slotIndex < 29) {
-                if (!this.moveItemStackTo(itemstack1, 29, 38, false)) {
-                    return ItemStack.EMPTY;
-                }
-            } else if (slotIndex >= 29 && slotIndex < 38 && !this.moveItemStackTo(itemstack1, 2, 29, false)) {
+    // THIS YOU HAVE TO DEFINE!
+    private static final int TE_INVENTORY_SLOT_COUNT = 3;  // must be the number of slots you have!
+
+    @Override
+    public ItemStack quickMoveStack(Player playerIn, int index) {
+        Slot sourceSlot = slots.get(index);
+        if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;  //EMPTY_ITEM
+        ItemStack sourceStack = sourceSlot.getItem();
+        ItemStack copyOfSourceStack = sourceStack.copy();
+
+        // Check if the slot clicked is one of the vanilla container slots
+        if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
+            // This is a vanilla container slot so merge the stack into the tile inventory
+            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
+                    + TE_INVENTORY_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;  // EMPTY_ITEM
+            }
+        } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
+            // This is a TE slot so merge the stack into the players inventory
+            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
                 return ItemStack.EMPTY;
             }
-
-            if (itemstack1.isEmpty()) {
-                slot.set(ItemStack.EMPTY);
-            }
-
-            slot.setChanged();
-            if (itemstack1.getCount() == itemstack.getCount()) {
-                return ItemStack.EMPTY;
-            }
-
-            slot.onTake(player, itemstack1);
-            this.broadcastChanges();
+        } else {
+            System.out.println("Invalid slotIndex:" + index);
+            return ItemStack.EMPTY;
         }
-
-        return itemstack;
+        // If stack size == 0 (the entire stack was moved) set slot contents to null
+        if (sourceStack.getCount() == 0) {
+            sourceSlot.set(ItemStack.EMPTY);
+        } else {
+            sourceSlot.setChanged();
+        }
+        sourceSlot.onTake(playerIn, sourceStack);
+        return copyOfSourceStack;
     }
+
 
     public void removed(Player player) {
         super.removed(player);
         this.resultContainer.removeItemNoUpdate(1);
         this.access.execute((p_40313_, p_40314_) -> {
             this.clearContainer(player, this.containerForBlock);
+            this.clearContainer(player, this.containerForExtra);
         });
     }
 
